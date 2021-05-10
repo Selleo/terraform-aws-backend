dfa435d081ec3071ced3cc288853387510b4b86a
Lukasz Pawlik <l.pawlik@selleo.com>
2021-05-10 11:42:17 +0200

    Add DNS name and Zone ID to Loadbalancer module output

--- modules/load-balancer/output.tf
+++ modules/load-balancer/output.tf
@@ -6,3 +6,10 @@ output "loadbalancer_sg_id" {
   value = aws_security_group.lb_sg.id
 }
 
+output "loadbalancer_dns_name" {
+  value = aws_alb.this.dns_name
+}
+
+output "loadbalancer_zone_id" {
+  value = aws_alb.this.zone_id
+}
